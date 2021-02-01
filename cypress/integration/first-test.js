a0947cbf5cda93e03d36eb5608342f4afb312601
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-02-01 19:17:05 +0100

    chore(section-4): add basic test

--- cypress/integration/first-test.js
+++ cypress/integration/first-test.js
@@ -1,5 +1,10 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', () => {
+    it('My First Test Case', () => {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4);
     })
 })
